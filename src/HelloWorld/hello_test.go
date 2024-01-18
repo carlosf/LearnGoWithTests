845d80302f6a639e8865c12977a053c4fdbe963f
Carlos Ferreira <carlosf@northrock.bm>
2024-01-17 23:12:49 -0400

    Completed Hello World portion of Learn go with tests

--- src/HelloWorld/hello_test.go
+++ src/HelloWorld/hello_test.go
@@ -1,1 +1,47 @@
 package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// t.Helper() is needed to tell the test suite that this method is a helper.
+	// By doing this when it fails the line number reported will be in our function call rather than inside our test helper.
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
