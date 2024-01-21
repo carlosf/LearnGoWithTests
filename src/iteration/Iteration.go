24153a97f19c8c8e0377800ba1b2b86871e03ad8
Carlos Ferreira <carlosf@northrock.bm>
2024-01-20 23:46:34 -0400

    Finished iterations.  More test cases, inlcuded examples and benchmarks

--- src/iteration/Iteration.go
+++ src/iteration/Iteration.go
@@ -1,5 +1,9 @@
 package iteration
 
-func Repeat(character string) string {
-	return ""
+func Repeat(character string, numOfTimes int) string {
+	var repeated string
+	for i := 0; i < numOfTimes; i++ {
+		repeated += character
+	}
+	return repeated
 }
