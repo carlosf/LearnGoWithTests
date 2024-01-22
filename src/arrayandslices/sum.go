468c6f6a6376b2cd8573260bb405cde2bbc240f8
Carlos Ferreira <carlosf@northrock.bm>
2024-01-21 22:40:11 -0400

    Working thru arrays and slics.

---
+++ src/arrayandslices/sum.go
@@ -0,0 +1,18 @@
+package arrayandslices
+
+func Sum(a []int) int {
+	sum := 0
+	for _, number := range a {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
